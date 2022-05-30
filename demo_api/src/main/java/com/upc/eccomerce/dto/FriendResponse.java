137d7f7f9d9cca929625982f9121101c4227ac70
sebasatarama <89918973+sebasatarama@users.noreply.github.com>
2022-05-30 00:01:08 -0500

    Delete demo_api directory

--- demo_api/src/main/java/com/upc/eccomerce/dto/FriendResponse.java
+++
@@ -1,9 +0,0 @@
-package com.upc.eccomerce.dto;
-
-import lombok.Data;
-
-@Data
-public class FriendResponse {
-    private Integer user1_id;
-    private Integer user2_id;
-}
--- demo_api/src/main/java/com/upc/eccomerce/util/RequestType.java
+++
@@ -1,5 +0,0 @@
-package com.upc.eccomerce.util;
-
-public enum RequestType {
-    Friend, Reto
-}
